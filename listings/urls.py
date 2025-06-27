8094f3cd6e74e753695890c1b43e05d8fb50a912
kaatre <helloworld00ff@gmail.com>
2025-06-27 13:29:02 +0545

    viewsets urls update

--- listings/urls.py
+++ listings/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import BrowseViewSet, ListingViewSet
+from .views import BrowseViewSet, ListingViewSet, ImageViewSet, CartViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'create-listing', ListingViewSet, basename='create-listing')
-
+router.register(r'images', ImageViewSet, basename='images')
+router.register(r'cart', CartViewSet, basename='cart')
 urlpatterns = [
     path('browse/', BrowseViewSet.as_view(), name='browse'),
     path('', include(router.urls)),
